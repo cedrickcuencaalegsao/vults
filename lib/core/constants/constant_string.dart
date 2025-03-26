ddecd7f4aefa7f3d8f28a1874960641e18882a61
Cedrick C. Alegsao <cedrickcuencaalegsao@gmail.com>
2025-03-26 11:38:34 +0800

    adding core files.

---
+++ lib/core/constants/constant_string.dart
@@ -0,0 +1,8 @@
+class ConstantString {
+  // font family
+  static const String fontFredoka = 'Fredoka';
+  static const String fontFredokaOne = 'FredokaOne';
+
+  // App name
+  static const String appName = 'Vults';
+}
