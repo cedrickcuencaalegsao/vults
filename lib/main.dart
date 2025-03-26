ac8f2ea30e649f90240fff0a58ac752a2c056e43
Cedrick C. Alegsao <cedrickcuencaalegsao@gmail.com>
2025-03-26 12:14:49 +0800

    configuring firebase and adding dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,13 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import './views/screens/mobile/splash_screen.dart' as mobile;
 import './views/screens/web/splash_screen.dart' as web;
 
-void main() {
+// Firebase.
+import 'package:firebase_core/firebase_core.dart';
+import './firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
