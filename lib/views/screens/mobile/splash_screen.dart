be7df326e1630e21fe16a98f79809d0db6a5b4e0
Cedrick C. Alegsao <cedrickcuencaalegsao@gmail.com>
2025-03-30 11:12:35 +0800

    [0001] Splash Screen

--- lib/views/screens/mobile/splash_screen.dart
+++ lib/views/screens/mobile/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './landing_screen.dart';
+import '../../../core/constants/constant_string.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -7,12 +9,44 @@ class SplashScreen extends StatefulWidget {
   SplashScreenState createState() => SplashScreenState();
 }
 
-class SplashScreenState extends State<SplashScreen>{
+class SplashScreenState extends State<SplashScreen> {
   @override
-  Widget build(BuildContext context){
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const LandingScreen()),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Splash Screen Mobile.'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Image(
+              image: AssetImage('assets/icons/vultsicon.png'),
+              width: 200,
+              height: 200,
+            ),
+            Text(
+              ConstantString.appName,
+              style: TextStyle(
+                fontFamily: ConstantString.fontFredokaOne,
+                fontSize: 30,
+                color: ConstantString.darkBlue,
+              ),
+            ),
+            SizedBox(height: 150),
+            CircularProgressIndicator(),
+          ],
+        ),
       ),
     );
   }
